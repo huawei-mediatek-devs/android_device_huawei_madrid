bcd01198ab87ea54fe24f2e9e1e19b1d8a9f19d0
R0rt1z2 <me@r0rt1z2.com>
2024-03-02 18:14:18 +0100

    madrid: Initial skeleton

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_madrid.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_madrid-user \
+    lineage_madrid-userdebug \
+    lineage_madrid-eng
---
+++ lineage_madrid.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from madrid device
+$(call inherit-product, device/huawei/madrid/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := madrid
+PRODUCT_NAME := lineage_madrid
+PRODUCT_BRAND := HUAWEI
+PRODUCT_MODEL := MRD-LX1
+PRODUCT_MANUFACTURER := HUAWEI
+
+PRODUCT_GMS_CLIENTID_BASE := android-huawei
